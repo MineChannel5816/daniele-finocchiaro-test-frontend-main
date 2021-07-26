015aa156890e89b9de19419d76bd793b93f4cc26
MineChannel5816 <danielefinocchiaro@keix.com>
2021-07-26 17:46:11 +0200

    Corrected Imports, Added Support to BoilerCard

--- src/Pagination.tsx
+++ src/Pagination.tsx
@@ -1,9 +1,15 @@
-import classNames from 'classnames';
 import React, { BaseSyntheticEvent, useState } from 'react';
+
+import type { BoilerInfo } from './App';
+
+import classNames from 'classnames';
+
 import { BoilerCard } from './BoilerCard';
 
 export interface BoilerCard {
+  cart: BoilerInfo[];
   id: string;
+  setToCart: (id: BoilerInfo[]) => void;
   discount: boolean;
   imageSrc: string;
   articleName: string;
@@ -14,9 +20,11 @@ export interface BoilerCard {
 }
 
 export function Pagination(props: {
-  data: BoilerCard[];
+  data: BoilerInfo[];
   pageLimit: number;
   dataLimit: number;
+  cart: BoilerInfo[];
+  onClickItems: (val: BoilerInfo[]) => void;
 }) {
   const pages = Math.max(1, Math.round(props.data.length / props.dataLimit));
   const [currentPage, setCurrentPage] = useState(1);
@@ -66,45 +74,55 @@ export function Pagination(props: {
 
   return (
     <>
-      <div className="m-24 flex justify-around flex-wrap">
-        {getPaginatedData().map((cardData, i) => (
-          <BoilerCard key={i} {...cardData} />
-        ))}
-      </div>
+      <div>
+        <div className="flex justify-around flex-wrap">
+          {getPaginatedData().map((cardData, i) => {
+            let propsToCard: BoilerCard = {
+              ...cardData,
+              cart: props.cart,
+              setToCart: props.onClickItems,
+            };
 
-      <div className="flex items-center justify-between fixed bottom-0 w-full pl-96">
-        <button
-          onClick={goToPreviousPage}
-          disabled={currentPage === 1 ? true : false}
-          className="text-xl text-indigo-800 p-3 cursor-pointer m-4 text-center focus:outline-none disabled:text-blueGray-700 disabled:cursor-not-allowed disabled:border-blueGray-400 disabled:opacity-50"
-        >
-          {'<'} Prev
-        </button>
-
-        <div>
-          {getPaginationGroup().map((page, i) => (
-            <button
-              key={i}
-              onClick={changePage}
-              className={classNames(
-                `w-10 h-10 font-medium rounded-full m-2 focus:outline-none`,
-                currentPage === page
-                  ? 'bg-indigo-600 text-white shadow-xl'
-                  : 'text-gray-600',
-              )}
-            >
-              <span>{page}</span>
-            </button>
-          ))}
+            return <BoilerCard key={i} {...propsToCard} />;
+          })}
         </div>
 
-        <button
-          onClick={goToNextPage}
-          disabled={currentPage === pages ? true : false}
-          className="text-xl text-indigo-800 p-3 cursor-pointer m-4 text-center focus:outline-none disabled:text-blueGray-700 disabled:cursor-not-allowed disabled:border-blueGray-400 disabled:opacity-50"
-        >
-          Next {'>'}
-        </button>
+        <hr className="mt-12 mx-6" />
+
+        <div className="flex items-center justify-between">
+          <button
+            onClick={goToPreviousPage}
+            disabled={currentPage === 1 ? true : false}
+            className="text-xl text-indigo-800 p-3 cursor-pointer m-4 text-center focus:outline-none disabled:text-blueGray-700 disabled:cursor-not-allowed disabled:border-blueGray-400 disabled:opacity-50"
+          >
+            {'<'} Prev
+          </button>
+
+          <div>
+            {getPaginationGroup().map((page, i) => (
+              <button
+                key={i}
+                onClick={changePage}
+                className={classNames(
+                  `w-10 h-10 font-medium rounded-full m-2 focus:outline-none`,
+                  currentPage === page
+                    ? 'bg-indigo-600 text-white shadow-xl'
+                    : 'text-gray-600',
+                )}
+              >
+                <span>{page}</span>
+              </button>
+            ))}
+          </div>
+
+          <button
+            onClick={goToNextPage}
+            disabled={currentPage === pages ? true : false}
+            className="text-xl text-indigo-800 p-3 cursor-pointer m-4 text-center focus:outline-none disabled:text-blueGray-700 disabled:cursor-not-allowed disabled:border-blueGray-400 disabled:opacity-50"
+          >
+            Next {'>'}
+          </button>
+        </div>
       </div>
     </>
   );
