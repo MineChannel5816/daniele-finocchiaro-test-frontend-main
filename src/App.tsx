2d86a8afb92f85fb012c2355d1b2d5b8969b30b5
MineChannel5816 <danielefinocchiaro@keix.com>
2021-07-26 17:47:17 +0200

    Added Cart List, Fixed Imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
-import React from 'react';
-
-import _ from 'lodash';
+import React, { useEffect, useState } from 'react';
 
 import * as Blueprint from '@blueprintjs/core';
 
@@ -17,6 +15,201 @@ import { Pagination } from './Pagination';
 import { SideBar } from './SideBar';
 
 import { v4 } from 'uuid';
+import { Footer } from './Footer';
+
+export interface BoilerInfo {
+  id: string;
+  discount: boolean;
+  imageSrc: string;
+  articleName: string;
+  articleDescription: string;
+  numberStar: number;
+  price: number;
+  oldPrice?: number;
+}
+
+const dataBoiler: BoilerInfo[] = [
+  {
+    id: v4(),
+    discount: true,
+    imageSrc: boiler,
+    articleName: 'Ariston',
+    articleDescription:
+      'Matis condens - Condensing Boiler 24 kW Methane - Indoor',
+    numberStar: 5,
+    price: 1350,
+    oldPrice: 1570,
+  },
+  {
+    id: v4(),
+    discount: false,
+    imageSrc: boiler,
+    articleName: 'Baxi',
+    articleDescription: 'Genus One Condensing Boiler - 24 kW',
+    numberStar: 2,
+    price: 960,
+  },
+  {
+    id: v4(),
+    discount: true,
+    imageSrc: boiler,
+    articleName: 'Ariston',
+    articleDescription:
+      'Matis condens - Condensing Boiler 24 kW Methane - Indoor',
+    numberStar: 5,
+    price: 1350,
+    oldPrice: 1570,
+  },
+  {
+    id: v4(),
+    discount: false,
+    imageSrc: boiler,
+    articleName: 'Baxi',
+    articleDescription: 'Genus One Condensing Boiler - 24 kW',
+    numberStar: 2,
+    price: 960,
+  },
+  {
+    id: v4(),
+    discount: true,
+    imageSrc: boiler,
+    articleName: 'Ariston',
+    articleDescription:
+      'Matis condens - Condensing Boiler 24 kW Methane - Indoor',
+    numberStar: 5,
+    price: 1350,
+    oldPrice: 1570,
+  },
+  {
+    id: v4(),
+    discount: false,
+    imageSrc: boiler,
+    articleName: 'Baxi',
+    articleDescription: 'Genus One Condensing Boiler - 24 kW',
+    numberStar: 2,
+    price: 960,
+  },
+  {
+    id: v4(),
+    discount: true,
+    imageSrc: boiler,
+    articleName: 'Ariston',
+    articleDescription:
+      'Matis condens - Condensing Boiler 24 kW Methane - Indoor',
+    numberStar: 5,
+    price: 1350,
+    oldPrice: 1570,
+  },
+  {
+    id: v4(),
+    discount: false,
+    imageSrc: boiler,
+    articleName: 'Baxi',
+    articleDescription: 'Genus One Condensing Boiler - 24 kW',
+    numberStar: 2,
+    price: 960,
+  },
+  {
+    id: v4(),
+    discount: true,
+    imageSrc: boiler,
+    articleName: 'Ariston',
+    articleDescription:
+      'Matis condens - Condensing Boiler 24 kW Methane - Indoor',
+    numberStar: 5,
+    price: 1350,
+    oldPrice: 1570,
+  },
+  {
+    id: v4(),
+    discount: false,
+    imageSrc: boiler,
+    articleName: 'Baxi',
+    articleDescription: 'Genus One Condensing Boiler - 24 kW',
+    numberStar: 2,
+    price: 960,
+  },
+  {
+    id: v4(),
+    discount: true,
+    imageSrc: boiler,
+    articleName: 'Ariston',
+    articleDescription:
+      'Matis condens - Condensing Boiler 24 kW Methane - Indoor',
+    numberStar: 5,
+    price: 1350,
+    oldPrice: 1570,
+  },
+  {
+    id: v4(),
+    discount: false,
+    imageSrc: boiler,
+    articleName: 'Baxi',
+    articleDescription: 'Genus One Condensing Boiler - 24 kW',
+    numberStar: 2,
+    price: 960,
+  },
+  {
+    id: v4(),
+    discount: true,
+    imageSrc: boiler,
+    articleName: 'Ariston',
+    articleDescription:
+      'Matis condens - Condensing Boiler 24 kW Methane - Indoor',
+    numberStar: 5,
+    price: 1350,
+    oldPrice: 1570,
+  },
+  {
+    id: v4(),
+    discount: false,
+    imageSrc: boiler,
+    articleName: 'Baxi',
+    articleDescription: 'Genus One Condensing Boiler - 24 kW',
+    numberStar: 2,
+    price: 960,
+  },
+  {
+    id: v4(),
+    discount: true,
+    imageSrc: boiler,
+    articleName: 'Ariston',
+    articleDescription:
+      'Matis condens - Condensing Boiler 24 kW Methane - Indoor',
+    numberStar: 5,
+    price: 1350,
+    oldPrice: 1570,
+  },
+  {
+    id: v4(),
+    discount: false,
+    imageSrc: boiler,
+    articleName: 'Baxi',
+    articleDescription: 'Genus One Condensing Boiler - 24 kW',
+    numberStar: 2,
+    price: 960,
+  },
+  {
+    id: v4(),
+    discount: true,
+    imageSrc: boiler,
+    articleName: 'Ariston',
+    articleDescription:
+      'Matis condens - Condensing Boiler 24 kW Methane - Indoor',
+    numberStar: 5,
+    price: 1350,
+    oldPrice: 1570,
+  },
+  {
+    id: v4(),
+    discount: false,
+    imageSrc: boiler,
+    articleName: 'Baxi',
+    articleDescription: 'Genus One Condensing Boiler - 24 kW',
+    numberStar: 2,
+    price: 960,
+  },
+];
 
 function App() {
   let toaster: Blueprint.Toaster;
@@ -28,198 +221,27 @@ function App() {
     toaster.show({ message: 'Added!', intent: 'success' });
   };
 
-  const dataBoiler = [
-    {
-      id: v4(),
-      discount: true,
-      imageSrc: boiler,
-      articleName: 'Ariston',
-      articleDescription:
-        'Matis condens - Condensing Boiler 24 kW Methane - Indoor',
-      numberStar: 5,
-      price: 1350,
-      oldPrice: 1570,
-    },
-    {
-      id: v4(),
-      discount: false,
-      imageSrc: boiler,
-      articleName: 'Baxi',
-      articleDescription: 'Genus One Condensing Boiler - 24 kW',
-      numberStar: 2,
-      price: 960,
-    },
-    {
-      id: v4(),
-      discount: true,
-      imageSrc: boiler,
-      articleName: 'Ariston',
-      articleDescription:
-        'Matis condens - Condensing Boiler 24 kW Methane - Indoor',
-      numberStar: 5,
-      price: 1350,
-      oldPrice: 1570,
-    },
-    {
-      id: v4(),
-      discount: false,
-      imageSrc: boiler,
-      articleName: 'Baxi',
-      articleDescription: 'Genus One Condensing Boiler - 24 kW',
-      numberStar: 2,
-      price: 960,
-    },
-    {
-      id: v4(),
-      discount: true,
-      imageSrc: boiler,
-      articleName: 'Ariston',
-      articleDescription:
-        'Matis condens - Condensing Boiler 24 kW Methane - Indoor',
-      numberStar: 5,
-      price: 1350,
-      oldPrice: 1570,
-    },
-    {
-      id: v4(),
-      discount: false,
-      imageSrc: boiler,
-      articleName: 'Baxi',
-      articleDescription: 'Genus One Condensing Boiler - 24 kW',
-      numberStar: 2,
-      price: 960,
-    },
-    {
-      id: v4(),
-      discount: true,
-      imageSrc: boiler,
-      articleName: 'Ariston',
-      articleDescription:
-        'Matis condens - Condensing Boiler 24 kW Methane - Indoor',
-      numberStar: 5,
-      price: 1350,
-      oldPrice: 1570,
-    },
-    {
-      id: v4(),
-      discount: false,
-      imageSrc: boiler,
-      articleName: 'Baxi',
-      articleDescription: 'Genus One Condensing Boiler - 24 kW',
-      numberStar: 2,
-      price: 960,
-    },
-    {
-      id: v4(),
-      discount: true,
-      imageSrc: boiler,
-      articleName: 'Ariston',
-      articleDescription:
-        'Matis condens - Condensing Boiler 24 kW Methane - Indoor',
-      numberStar: 5,
-      price: 1350,
-      oldPrice: 1570,
-    },
-    {
-      id: v4(),
-      discount: false,
-      imageSrc: boiler,
-      articleName: 'Baxi',
-      articleDescription: 'Genus One Condensing Boiler - 24 kW',
-      numberStar: 2,
-      price: 960,
-    },
-    {
-      id: v4(),
-      discount: true,
-      imageSrc: boiler,
-      articleName: 'Ariston',
-      articleDescription:
-        'Matis condens - Condensing Boiler 24 kW Methane - Indoor',
-      numberStar: 5,
-      price: 1350,
-      oldPrice: 1570,
-    },
-    {
-      id: v4(),
-      discount: false,
-      imageSrc: boiler,
-      articleName: 'Baxi',
-      articleDescription: 'Genus One Condensing Boiler - 24 kW',
-      numberStar: 2,
-      price: 960,
-    },
-    {
-      id: v4(),
-      discount: true,
-      imageSrc: boiler,
-      articleName: 'Ariston',
-      articleDescription:
-        'Matis condens - Condensing Boiler 24 kW Methane - Indoor',
-      numberStar: 5,
-      price: 1350,
-      oldPrice: 1570,
-    },
-    {
-      id: v4(),
-      discount: false,
-      imageSrc: boiler,
-      articleName: 'Baxi',
-      articleDescription: 'Genus One Condensing Boiler - 24 kW',
-      numberStar: 2,
-      price: 960,
-    },
-    {
-      id: v4(),
-      discount: true,
-      imageSrc: boiler,
-      articleName: 'Ariston',
-      articleDescription:
-        'Matis condens - Condensing Boiler 24 kW Methane - Indoor',
-      numberStar: 5,
-      price: 1350,
-      oldPrice: 1570,
-    },
-    {
-      id: v4(),
-      discount: false,
-      imageSrc: boiler,
-      articleName: 'Baxi',
-      articleDescription: 'Genus One Condensing Boiler - 24 kW',
-      numberStar: 2,
-      price: 960,
-    },
-    {
-      id: v4(),
-      discount: true,
-      imageSrc: boiler,
-      articleName: 'Ariston',
-      articleDescription:
-        'Matis condens - Condensing Boiler 24 kW Methane - Indoor',
-      numberStar: 5,
-      price: 1350,
-      oldPrice: 1570,
-    },
-    {
-      id: v4(),
-      discount: false,
-      imageSrc: boiler,
-      articleName: 'Baxi',
-      articleDescription: 'Genus One Condensing Boiler - 24 kW',
-      numberStar: 2,
-      price: 960,
-    },
-  ];
+  const [cartItems, setCartItems] = useState<BoilerInfo[]>([]);
+
+  useEffect(() => {
+    console.log(cartItems, cartItems.length);
+  }, [cartItems]);
 
   return (
     <>
       <Blueprint.Toaster position={Blueprint.Position.TOP} ref={refHandlers} />
-      <div className="flex">
+      <div className="flex mb-12">
         <SideBar />
-        <Pagination data={dataBoiler} pageLimit={3} dataLimit={12} />
+        <Pagination
+          data={dataBoiler}
+          cart={cartItems}
+          onClickItems={setCartItems}
+          pageLimit={3}
+          dataLimit={12}
+        />
       </div>
+      {cartItems.length > 0 ? <Footer cartItems={cartItems} /> : null}
 
-      <hr />
       {/* <TransactionEmissionPanel handleToast={addToast} /> */}
     </>
   );
